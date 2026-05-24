525e838d6fc2e0546fc7392af1efa7688fe53968
Priyanshu Kumar <shivamkumar3214rew@gmail.com>
2026-05-25 02:45:27 +0530

    used Jsx for first time

--- react.js
+++ react.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-const element = React.createElement("h2", { id: "toot", style: { color: "red", backgroundColor: "black", fontSize: "Arial" } }, "hello kahan hai");
-const element2 = React.createElement("h2", { id: "voot", style: { color: "red", backgroundColor: "black", fontSize: "Arial" } }, "Yahan hai hum");
-const div = React.createElement("h2", { id: "loot", style: { color: "red", backgroundColor: "black", fontSize: "Arial" } }, [element, element2]);
-
+// const element = React.createElement("h2", { id: "toot", style: { color: "red", backgroundColor: "black", fontSize: "Arial" } }, "hello kahan hai");
+// const element2 = React.createElement("h2", { id: "voot", style: { color: "red", backgroundColor: "black", fontSize: "Arial" } }, "Yahan hai hum");
+// const div = React.createElement("h2", { id: "loot", style: { color: "red", backgroundColor: "black", fontSize: "Arial" } }, [element, element2]);
+// here createElement change it into a js object or react element 
 const Reactroot = createRoot(document.querySelector("#root"));
-Reactroot.render(div);
+
 // Reactroot.render(element);
 // // you will see that the first element get replaced by second element so instead of this let's try another method 
 // Reactroot.render(element2);
@@ -16,6 +16,35 @@ Reactroot.render(div);
 // so we use Bundler
 // bundler utna hi code dega jitna tumko chahiye na jyada na kam 
  // bundler can be webpack, vite , Parcel and webpack
+
+ //jsx is different from html because it can take javascript expression 
+ // expressions are the final result of the code it will not take statement 
+
+ // you can also just style it from making a css document 
+
+ // three ways to add a style in the jsx 
+
+ const style={
+    backgroundColor:"red",
+    color:"black",
+    fontFamily:"arial"
+ }
+
+ const newElement = (
+    <div>
+      <h1
+        id="hello"
+        className="hello2"
+        style={{ backgroundColor: "black", color: "red", fontFamily: "Arial" }}
+      >
+        hello ,hi ,bye bye
+      </h1>
+      <h2 id="hello3" className="hello4" style={style}>what are u doing man</h2>
+      <h3 id="first">i am doing nothing man</h3>
+    </div>
+ );
+ 
+ Reactroot.render(newElement);
 
 
 
